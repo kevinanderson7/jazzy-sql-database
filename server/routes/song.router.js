5acdf890773ea44e229d2b485fab4ca867bd7a9e
Kevin Anderson <kmichael419@hotmail.com>
2020-07-07 16:14:09 -0500

    GET route working for songs

--- server/routes/song.router.js
+++ server/routes/song.router.js
@@ -1,28 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
 // static content. this will be replaced with a database table
-const songListArray = [
-    {
-        title: 'Take Five',
-        length: '2:55',
-        date_released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        date_released: '1959-08-17'
-    }
-];
+// const songListArray = [
+//     {
+//         title: 'Take Five',
+//         length: '2:55',
+//         date_released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         date_released: '1959-08-17'
+//     }
+// ];
 
 router.get('/', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(songListArray);
+  const queryText = `SELECT * FROM "songs" ORDER BY "title" ASC;`;
+
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      res.send(dbResponse.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 router.post('/', (req, res) => {
-    
-    res.sendStatus(201);
+  res.sendStatus(201);
 });
 
 module.exports = router;
