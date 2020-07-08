2d196f41fe77124e076769c4f717af9248e6d7d2
Kevin Anderson <kmichael419@hotmail.com>
2020-07-08 09:45:04 -0500

    updating project sql file to include text entered into postico

--- database.sql
+++ database.sql
@@ -10,3 +10,9 @@ CREATE TABLE "songs" (
 	"song_length" varchar(80),
 	"date_released" date
 );
+
+INSERT INTO "songs" ("title", "song_length", "date_released")
+VALUES ('Smells Like Teen Spirit', '2:00', '1/1/1991');
+
+INSERT INTO "songs" ("title", "song_length", "date_released")
+VALUES ('U Can''t Touch This', '2:50', '1/1/1990');
